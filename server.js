a884cc4e94a98bde7156818c3cba9f9de2714252
Shwetank <shwetankawasthi97@gmail.com>
2018-02-25 15:24:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
+var config = {
+    user : 'shwetankawasthi97',
+    database : 'shwetankawasthi97',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
+var pool = new Pool(config);
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -42,6 +54,18 @@ function createTemplate(data){
     title = data.title;
     content = data.content;
 
+app.get('/test-db', function(req, res){
+    // make a select request
+    // return a response
+    pool.query('Select * from test', function(err, result){
+        if(err){
+            res(500).send(err.toString());
+        }
+        else{
+            send(JSON.stringify(result));
+        }
+    });
+});
 
 var htmlTemplate =`
     <html>
